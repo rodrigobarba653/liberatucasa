b7ecaa58316731b25ba380b7ee704ec26c602e04
rodrigobarba653 <rodrigobarba653@gmail.com>
2025-07-24 13:13:30 -0600

    Fix: Form-Colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-white ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
